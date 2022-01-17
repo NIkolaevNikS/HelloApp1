ae6ed86f2662d08006e22e9be3bf7e8273ddcb84
NIkolaevNikS <nikita.nikolaevwork@gmail.com>
2022-01-17 19:37:41 +0300

    Initial commit

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,18 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double a = 9;
+
+        double b = 12;
+
+        double c;
+
+        c = Math.sqrt(a * a + b * b);
+
+        System.out.println("c = " + c);
+
+    }
+}
